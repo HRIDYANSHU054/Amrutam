418cdce47cea8089e6ec55a3f990b919e1c26723
Hridyanshu7 <himank7794@gmail.com>
2024-12-26 21:48:20 +0530

    change: DoctorCard Container now only supports upto 3 columns

--- app/(with-header)/doctors/page.tsx
+++ app/(with-header)/doctors/page.tsx
@@ -127,7 +127,7 @@ function FindDoctorPage({}) {
         </div>
 
         {/* cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-12 mx-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3   gap-12 mx-20">
           <DoctorCard />
           <DoctorCard />
           <DoctorCard />
